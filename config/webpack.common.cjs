170f9d8c34494c87f28c8da94f69b5a0e84d0531
Rodrigo Andre Gutierrez <rodrigoandregutierrez@gmail.com>
2026-02-04 13:36:12 -0300

    Build: Final stable production-ready portfolio

--- config/webpack.common.cjs
+++ config/webpack.common.cjs
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: '[name].[contenthash].js',
-    publicPath: '/',
+    publicPath: "auto",
     clean: true,
   },
   module: {
